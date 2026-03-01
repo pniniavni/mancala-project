4982a99cc4838215787e59d4e2d0d5996d295e87
p0556738039@gmail.com <p0556738039@gmail.com>
2026-03-01 02:05:37 +0200

    חיבור ועיצוב דף כניסה

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (event) => {
+   
+    event.preventDefault();
+
+
+    const username = document.querySelector('#username').value;
+    const level = document.querySelector('#level').value;
+
+
+    localStorage.setItem('currentPlayer', username);
+    localStorage.setItem('gameLevel', level);
+
+  
+    window.location.href = 'pages/game.html';
+});
