aa4f44726f214f9bf6702b98457143d7c45abc57
p0556738039@gmail.com <p0556738039@gmail.com>
2026-03-01 02:37:41 +0200

    תיקון הטופס של הלחיצה

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,12 @@
-
-
 const loginForm = document.querySelector('#login-form');
 
 loginForm.addEventListener('submit', (event) => {
-   
     event.preventDefault();
-
-
     const username = document.querySelector('#username').value;
     const level = document.querySelector('#level').value;
-
 
     localStorage.setItem('currentPlayer', username);
     localStorage.setItem('gameLevel', level);
 
-  
-    window.location.href = 'pages/game.html';
+    window.location.href = "pages/game.html";
 });
