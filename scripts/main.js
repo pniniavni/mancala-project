ce06e76bf3c2a09d45a8eb361e44306ccd2da822
p0556738039@gmail.com <p0556738039@gmail.com>
2026-03-01 02:55:18 +0200

    מעבר דף עובר תיקון

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector('#login-form');
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
+
     const username = document.querySelector('#username').value;
     const level = document.querySelector('#level').value;
 
